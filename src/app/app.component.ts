fa779f45cf715e3e7cf873f8a69b19ebb83b30ad
Carles Huix Vidal <54493791+carleshv70@users.noreply.github.com>
2024-06-29 09:33:21 +0200

    Crear una aplicació SSR utilitzant Angular Universal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from './github.service';
+import { Title, Meta } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -11,10 +13,23 @@ export class AppComponent implements OnInit {
   username = "";
 
    constructor(
-    private service: GithubService
+    private service: GithubService,
+    private titleService: Title,
+    private meta: Meta
    ) {}
 
    ngOnInit(): void {
       this.username = this.service.username;
+      this.titleService.setTitle('GitHub portfolio app');
+      this.meta.addTags([
+        {
+          name: 'description',
+          content: `${this.username}'s GitHub portfolio`
+        },
+        {
+          name: 'author',
+          content: this.username
+        }
+      ]);
    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { PanelComponent } from './panel/panel.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
 import { OrganizationsComponent } from './organizations/organizations.component';
+import { TransferHttpCacheModule } from '@nguniversal/common';
+
 
 @NgModule({
   declarations: [
@@ -16,8 +18,9 @@ import { OrganizationsComponent } from './organizations/organizations.component'
     OrganizationsComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     HttpClientModule,
+    TransferHttpCacheModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
